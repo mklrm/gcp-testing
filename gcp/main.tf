01c030211e86f878dcc55dbe4f0e2b4c64f6c065
mklrm <25104464+mklrm@users.noreply.github.com>
2023-01-31 20:35:36 +0200

    Reconsidering name generation rules. Or rather considering it for the first time.

--- gcp/main.tf
+++ gcp/main.tf
@@ -14,6 +14,94 @@ provider "google" {
   # Configuration options
 }
 
+# Name generation design
+#
+# Values:
+# network_name = cucumber
+# 
+# Generated names:
+#                        network_name: cucumber
+#
+#                   subnetwork_0_name: cucumber-subnet-0
+# subnetwork_0_secondary_range_name_0: cucumber-subnet-0-secondary-range-0
+# subnetwork_0_secondary_range_name_1: cucumber-subnet-0-secondary-range-1
+#
+#                   subnetwork_0_name: cucumber-subnet-1
+#
+#--------------------------------------------------------------------------------#
+#
+# Values:
+# network_name_prefix = tomato
+#
+# Generated names:
+#                        network_name: tomato-network
+#
+#                   subnetwork_0_name: tomato-network-subnet-0
+# subnetwork_0_secondary_range_name_0: tomato-network-subnet-0-secondary-range-0
+# subnetwork_0_secondary_range_name_1: tomato-network-subnet-0-secondary-range-1
+#
+#                   subnetwork_1_name: tomato-network-subnet-1
+#
+#--------------------------------------------------------------------------------#
+#
+# Values:
+#          network_name_prefix = olive
+# network_name_postfix_disable = true
+#
+# Generated names:
+#                        network_name: olive
+#
+#                   subnetwork_0_name: olive-subnet-0
+# subnetwork_0_secondary_range_name_0: olive-subnet-0-secondary-range-0
+# subnetwork_0_secondary_range_name_1: olive-subnet-0-secondary-range-1
+#
+#                   subnetwork_1_name: olive-subnet-1
+#
+#--------------------------------------------------------------------------------#
+#
+# Values:
+#                    network_name_prefix = potato
+#           network_name_postfix_disable = true
+#               subnetwork_0_name_prefix = banana
+# subnetwork_1_secondary_ip_range_prefix = kiwi
+#
+# Generated names:
+#                        network_name: potato
+#
+#                   subnetwork_0_name: potato-banana-0
+# subnetwork_0_secondary_range_name_0: potato-banana-0-secondary-range-0
+# subnetwork_0_secondary_range_name_1: potato-banana-0-secondary-range-1
+#
+#                   subnetwork_1_name: potato-subnet-1
+# subnetwork_1_secondary_range_name_0: potato-subnet-0-kiwi-0
+# subnetwork_1_secondary_range_name_1: potato-subnet-0-kiwi-1
+#
+#--------------------------------------------------------------------------------#
+#
+# Values:
+#                            network_name_prefix = potato
+#                   network_name_postfix_disable = true
+#               subnetwork_0_name_prefix_disable = true
+# subnetwork_1_secondary_ip_range_prefix_disable = true
+#
+# Generated names:
+#                        network_name: potato
+#
+#                   subnetwork_0_name: potato-banana-0
+# subnetwork_0_secondary_range_name_0: potato-0-secondary-range-0
+# subnetwork_0_secondary_range_name_1: potato-0-secondary-range-1
+#
+#                   subnetwork_1_name: potato-subnet-1
+# subnetwork_1_secondary_range_name_0: potato-subnet-0-0
+# subnetwork_1_secondary_range_name_1: potato-subnet-0-1
+#
+#--------------------------------------------------------------------------------#
+#
+# Notes
+# - Remove postfix and postfix_disable from subnetwork and secondary_range, not worth the trouble
+# - Add disable_prefix to subnet and secondary ip range
+#
+
 module "dynamic_deployment" {
   source = "./module"
   compute_networks = [
